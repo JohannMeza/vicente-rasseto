ae49952a74a40fe3d0e5cc5bd2b75f7175f2ce5d
JohannMeza <johanmezasalazar@gmail.com>
2022-07-16 20:32:26 -0500

    Cambios

--- server/config/connection.js
+++ server/config/connection.js
@@ -3,6 +3,7 @@ const config = require("./config");
 
 const startConnection = async () => {
   try {
+    console.log("URI de la BD", config.database.MONGODB_URI)
     await connect(config.database.MONGODB_URI)
     console.log("Database is conected successfully");
   } catch (err) {
