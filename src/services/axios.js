b626fb69058b05e898ae93d059ee31c17bf6f6ae
JohannMeza <johanmezasalazar@gmail.com>
2022-12-07 20:45:09 -0500

    Version 1.0 New Deploy

--- src/services/axios.js
+++ src/services/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const axiosConfig = axios.create({
-  baseURL: "https://biblioteca-virtual-peru.herokuapp.com/api",
+  baseURL: "https://vicente-rasseto.onrender.com/api",
   // baseURL: "http://localhost:4010/api",
   header: {
     'Content-Type': 'application/json'
