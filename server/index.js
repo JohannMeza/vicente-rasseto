c98acfa59753f6f2bb086120c8637500666889f4
JohannMeza <johanmezasalazar@gmail.com>
2023-05-12 17:47:32 -0500

    Cambio webviewer

--- server/index.js
+++ server/index.js
@@ -2,6 +2,16 @@ const app = require("./app.js");
 const startConnection = require("./config/connection");
 startConnection();
 
+const fs = require('fs');
+
+fs.readdir("../", function (err, archivos) {
+if (err) {
+  console.log(err)
+return;
+}
+console.log(archivos);
+});
+
 app.listen(app.get("PORT"), () => {
   console.log("Ruta Base ", __dirname);
   console.log("Server on port", app.get("PORT"));
