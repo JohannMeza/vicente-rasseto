e3ec20a3f15d417bd4f8030e80536045a8734934
JohannMeza <johanmezasalazar@gmail.com>
2023-12-05 21:15:13 -0500

    Deploy

--- src/util/EnvConstant.js
+++ src/util/EnvConstant.js
@@ -1,5 +1,5 @@
 // Configuracion / desarrollo / produccion
-const REACT_APP_ENV                = "desarrollo"
+const REACT_APP_ENV                = "produccion"
 // Desarrollo
 const REACT_APP_DEV_PATH_UPLOAD    = "assets/upload/";
 const REACT_APP_DEV_BASE_URL       = "http://localhost:4010/api";
